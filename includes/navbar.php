c83c05f669b21d13712858492f062613e6d925e0
Lorenz Ciocon <lorenzciocon@gmail.com>
2025-10-07 22:25:47 +0800

    setup navbar

---
+++ includes/navbar.php
@@ -0,0 +1,11 @@
+<div class="nav-bar col-sm-2">
+    <h3>FluxTrack</h3>
+
+    <nav>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Active Incidents</a></li>
+            <li><a href="">Account</a></li>
+        </ul>
+    </nav>
+</div>
