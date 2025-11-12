e5eeef7ee5701ef595558852292708bc150f96f0
Lorenz Ciocon <lorenzciocon@gmail.com>
2025-11-12 17:44:53 +0800

    session redirect for login page

--- login-page.php
+++ login-page.php
@@ -6,6 +6,11 @@
   session_start();
   include('./includes/db_connect.php');
 
+  if (isset($_SESSION['user_id'])) {
+    header("Location: ./pages/dashboard.php");
+    exit;
+  }
+
   if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       $username = $_POST['username'] ?? '';
       $password = $_POST['password'] ?? '';
